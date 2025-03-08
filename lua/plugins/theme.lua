835e6ca5fbfe1b90a24ff96e0947401a642a8a58
zkx <zhxuankun@163.com>
2025-03-08 11:03:58 +0800

    fix(theme): use correct warning icon key in diagnostics indicator

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -14,7 +14,7 @@ return {
           diagnostics_indicator = function(_, _, diag)
             local icons = LazyVim.ui.icons.diagnostics
             local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-              .. (diag.warning and icons.Warn .. diag.warning or "")
+              .. (diag.warning and icons.Warning .. diag.warning or "")
             return vim.trim(ret)
           end,
           offsets = {
