77311589a7279187f8d1719f1c1329be37131aa5
zkx <zhxuankun@163.com>
2025-08-18 23:49:26 +0800

    feat(lsp): add inlay hint toggle keybinding, remove wrap/spell autocmd

--- lua/basic.lua
+++ lua/basic.lua
@@ -111,16 +111,6 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- wrap and check for spell in text filetypes
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("wrap_spell"),
-  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
-})
-
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = augroup("auto_create_dir"),
